5eb4b276f7ead38d91e18ace317cddfd2759d343
kkamara <itchykilljoy@protonmail.com>
2018-01-13 00:16:47 +0000

    Pushed images to public folder because storage/app folder changes don't push to github

--- resources/views/layouts/components/companyLogo.blade.php
+++ resources/views/layouts/components/companyLogo.blade.php
@@ -1,2 +1,2 @@
 
-    <img src="{{ asset('storage/images/human_resources.png') }}" alt="Company Logo" title="Company Logo" class="company-logo" class="img-responsive">
+    <img src="{{ asset('images/human_resources.png') }}" alt="Company Logo" title="Company Logo" class="company-logo" class="img-responsive">
--- resources/views/layouts/stylesheets.blade.php
+++ resources/views/layouts/stylesheets.blade.php
@@ -1,5 +1,5 @@
 {{--  Icons  --}}
-<link rel="shortcut icon" href="{{ asset('storage/images/favicon.ico') }}" type="image/x-icon">
+<link rel="shortcut icon" href="{{ asset('images/favicon.ico') }}" type="image/x-icon">
 
 <link rel="stylesheet" type="text/css" href="{{ asset('css/app.css') }}">
 <link rel="stylesheet" type="text/css" href="{{ asset('css/navbar.css') }}">
